e189d17dfab10c37b7b0f920ec791e0a0d233eec
alhaos <52729006+alhaos@users.noreply.github.com>
2022-12-23 07:55:31 +0500

    001

--- Source/AccuFinanceProvider/AccuFinanceProvider.go
+++ Source/AccuFinanceProvider/AccuFinanceProvider.go
@@ -1,1 +1,21 @@
 package AccuFinanceProvider
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type AccuFinanceProvider struct {
+	DB *sql.DB
+}
+
+func New() *AccuFinanceProvider {
+	afp := &AccuFinanceProvider{}
+	afp.DB = sql.Open("")
+
+	sql.Open("mssql", database.ConnectionString)
+	if connectionError != nil {
+		fmt.Println(fmt.Errorf("error opening database: %v", connectionError))
+	}
+	return afp
+}
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
 	"gopkg.in/yaml.v3"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 var Conf Config
@@ -24,26 +28,102 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	log.Println("Initialization complete")
 }
 
 func main() {
 
 	files, err := getFiles()
 	check(err)
+	log.Printf("%v files found in: %v \r\n", len(files), Conf.SourceDirectory)
 
 	err = truncate()
 	check(err)
+	log.Println("Buffer cleared")
 
 	err = dumpFiles(files)
 	check(err)
+	log.Println("Files dumped")
 
 	err = fillNewFiles()
 	check(err)
+	log.Println("New files filled")
+
+	newFiles, err := GetNewFiles()
+	check(err)
+
+	for _, file := range newFiles {
+		log.Printf("Found file: %v\r\n", file)
+		err := processNewFile(file)
+		check(err)
+	}
+
+	//accC19 := GetAccessionToSearch()
 }
 
-func fillNewFiles() error {
-	_, err := DB.Exec(`insert into FILES (NAME, IS_PROCESSED) select NAME, 0 from FILES_BUFFER fb where fb.NAME not in (select NAME from FILES)`)
+func processNewFile(filename string) (err error) {
+
+	file, err := os.Open(filename)
+
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	counter := 0
+	for scanner.Scan() {
+		if counter != 0 {
+			res := scanner.Text()
+			log.Println(res)
+			splits := strings.Split(res, ",")
+			if splits[1] == "C19" || splits[1] == "C19R" {
+				err = insertResult(filename, splits[0], splits[1], splits[2])
+				check(err)
+			}
+		}
+		counter++
+	}
+
+	if scanner.Err() != nil {
+		log.Fatal(scanner.Err())
+	}
+	err = commitFile(filename)
+	check(err)
+	return
+}
+
+func commitFile(filename string) (err error) {
+	_, err = DB.Exec("update files set IS_PROCESSED = 1 where name = ?", filename)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func insertResult(filename string, accession string, testName string, testResult string) (err error) {
+	_, err = DB.Exec("insert into RESULTS (FILENAME, ACCESSION, TESTNAME, TESTRESULT, DT, IS_PROCESSED) values (?,?,?,?,?,?)", filename, accession, testName, testResult, time.Now().Format("2006-01-02 15:04:05"), 0)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetNewFiles() (files []string, err error) {
+	rows, err := DB.Query("select NAME from FILES where IS_PROCESSED = 0")
+	check(err)
+	defer rows.Close()
+
+	for rows.Next() {
+		var name string
+		err = rows.Scan(&name)
+		check(err)
+		files = append(files, name)
+	}
+
+	return
+}
+
+func fillNewFiles() (err error) {
+	_, err = DB.Exec(`insert into FILES (NAME, IS_PROCESSED) select NAME, 0 from FILES_BUFFER fb where fb.NAME not in (select NAME from FILES)`)
 	if err != nil {
 		return err
 	}
@@ -76,7 +156,7 @@ func getFiles() (files []string, err error) {
 
 	for _, entry := range DirectoryEntry {
 		if !entry.IsDir() {
-			files = append(files, entry.Name())
+			files = append(files, filepath.Join(Conf.SourceDirectory, entry.Name()))
 		}
 	}
 	return
